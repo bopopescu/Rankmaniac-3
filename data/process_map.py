f291bc83fa6f78ed6a1ce27fc990b82f37e4e434
Gavy Aggarwal <gavyaggarwal@gmail.com>
2017-02-06 11:29:59 -0800

    Process Step

--- data/process_map.py
+++ data/process_map.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python
 
+import heapq
+import json
 import sys
 
-#
-# This program simply represents the identity function.
-#
+heap = []
+for i in range(20):
+    heapq.heappush(heap, (0.0, -1))
 
 for line in sys.stdin:
-    sys.stdout.write(line)
+    cmps = line.split("\t")
+    nodeID = int(cmps[0].split(":")[1])
+    rank = float(cmps[1].split(",")[0])
 
+    heapq.heappushpop(heap, (rank, nodeID))
+
+    #sys.stdout.write(line)
+sys.stdout.write("R\t" + json.dumps(heap) + "\n")
